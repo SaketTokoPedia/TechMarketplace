5b5c893ed19a69c30e5a48b865628d44e4390d4f
Rajmani Kushwaha <rajmanikush@gmail.com>
2023-08-26 16:04:52 +0530

    Initial Commit

---
+++ TechMarketplace/TechMarketplaceApp.swift
@@ -0,0 +1,20 @@
+//
+//  TechMarketplaceApp.swift
+//  TechMarketplace
+//
+//  Created by Rajmani Kushwaha on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TechMarketplaceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
